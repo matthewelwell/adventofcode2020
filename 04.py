1ff372dc780d91e29ca754e595415c1d496e86b0
Matthew Elwell <matthewe@solidstategroup.com>
2020-12-05 13:54:37 +0000

    Remove print statements

--- 04.py
+++ 04.py
@@ -2,10 +2,6 @@ from utils.day4.passport import Passport
 
 
 def part1(data):
-    passport_data = data.split("\n\n")
-    print(passport_data[0])
-    print(passport_data[-1])
-
     return sum(
         (
             1 if Passport(passport_data).has_required_attributes() else 0
