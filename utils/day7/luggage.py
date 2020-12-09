335aa9f8adeeecf372ea3dd604ed7645b1938363
Matthew Elwell <matthewe@solidstategroup.com>
2020-12-09 21:02:21 +0000

    Refactor to use class instead of tuple

--- utils/day7/luggage.py
+++ utils/day7/luggage.py
@@ -1,4 +1,14 @@
 import typing
+from dataclasses import dataclass
+
+
+@dataclass
+class Edge:
+    weight: int
+    colour: str
+
+    def __hash__(self):
+        return hash(f"{self.weight}_{self.colour}")
 
 
 def build_graph(data: str):
@@ -7,7 +17,7 @@ def build_graph(data: str):
     }
 
 
-def parse_input(input_: str) -> typing.Tuple[str, typing.Set[typing.Tuple]]:
+def parse_input(input_: str) -> typing.Tuple[str, typing.Set[Edge]]:
     node, edges_raw = input_.split("bags contain")
     edges = set()
     for child_bag_data in edges_raw.split(","):
@@ -18,7 +28,7 @@ def parse_input(input_: str) -> typing.Tuple[str, typing.Set[typing.Tuple]]:
         words.pop()  # to remove the keyword bag / bags from the end of the list
         quantity = int(words.pop(0))
         colour = " ".join(words)
-        edges.add((quantity, colour))
+        edges.add(Edge(weight=quantity, colour=colour))
 
     return node.strip(), edges
 
@@ -32,20 +42,20 @@ def count_suitable_containers(graph: dict, target: str):
 def can_contain_target(graph: dict, key: str, target: str):
     if not graph[key]:
         return False
-    if target in map(lambda edge: edge[1], graph[key]):
+    if target in map(lambda edge: edge.colour, graph[key]):
         return True
 
     return any(
-        can_contain_target(graph, child[1], target) for child in graph[key]
+        can_contain_target(graph, child.colour, target) for child in graph[key]
     )
 
 
 def count_child_bags(graph: dict, target: str) -> int:
-    return sum(key[0] * _count_child_bags(graph, key[1]) for key in graph[target])
+    return sum(key.weight * _count_child_bags(graph, key.colour) for key in graph[target])
 
 
 def _count_child_bags(graph: dict, key: str) -> int:
     count = 1
     for child in graph[key]:
-        count += child[0] * _count_child_bags(graph, child[1])
+        count += child.weight * _count_child_bags(graph, child.colour)
     return count
