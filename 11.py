day 11 - part 1 test case working, incorrect value for input

11.py
@@ -1,7 +1,18 @@
 import numpy as np
 
+from utils.day11.helpers import count_unique_values
+from utils.day11.waiting_area import WaitingArea, OCCUPIED
+
 
 def part1(data: str):
-    # convert the data into a numpy array
-    # todo: possibly we want to convert values to 1s and 0s
+    # convert the data into a numpy array of 1s (occupied), 0s (vacant) and None (floor)
     array = np.array([list(line) for line in data.splitlines()])
+
+    waiting_area = WaitingArea(seats=array)
+
+    updated = True
+    while updated:
+        updated = waiting_area.update_seats()
+
+    return count_unique_values(waiting_area.seats, OCCUPIED)
+